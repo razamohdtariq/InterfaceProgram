f535c31edf24a36aa95b8e71cb5ec9d8ac94641d
prashant-vaidwan038 <58844559+prashant-vaidwan038@users.noreply.github.com>
2022-02-18 17:53:15 +0530

    Interface

---
+++ src/Polygon.java
@@ -0,0 +1,23 @@
+
+public interface Polygon {
+	
+		  void getArea(int length, int breadth);
+		}
+
+		
+		class Rectangle implements Polygon {
+
+
+		  public void getArea(int length, int breadth) {
+			  
+		    System.out.println("The area of the rectangle is " + (length * breadth));
+		  }
+		}
+
+		class Main {
+		  public static void main(String args[]) {
+		    Rectangle r1 = new Rectangle();
+		    r1.getArea(8, 10);
+		  }
+		}
+	
